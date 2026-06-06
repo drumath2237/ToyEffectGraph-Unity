1ba26d29a2858438975eeb82ad112fef2941f2ea
drumath2237 <2237k.drumath@gmail.com>
2026-06-06 18:23:33 +0900

    :sparkles: create src for graph

---
+++ Assets/App/Scripts/Editor/ToyEffectGraph.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.GraphToolkit.Editor;
+
+namespace ToyEffectGraph.Editor
+{
+    [Serializable]
+    [Graph(AssetExtension)]
+    public class ToyEffectGraph : Graph
+    {
+        public const string AssetExtension = "toyvfx";
+    }
+}
