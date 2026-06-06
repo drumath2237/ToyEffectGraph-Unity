256c226c14c69052c234bcd1179f640faec9fab3
drumath2237 <2237k.drumath@gmail.com>
2026-06-06 19:50:31 +0900

    :sparkles: add block and context nodes

---
+++ Assets/App/Scripts/Editor/ContextNode.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Unity.GraphToolkit.Editor;
+using UnityEngine;
+
+namespace ToyEffectGraph.Editor
+{
+    [Serializable]
+    public class Spawn : ContextNode
+    {
+        public struct SpawnContextOutput
+        {
+        }
+
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddOutputPort<SpawnContextOutput>("").WithConnectorUI(PortConnectorUI.Arrowhead).Build();
+        }
+    }
+
+    [Serializable]
+    public class Initialize : ContextNode
+    {
+        public struct InitializeContextOutput
+        {
+        }
+
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddInputPort<Spawn.SpawnContextOutput>("").WithConnectorUI(PortConnectorUI.Arrowhead).Build();
+            context.AddOutputPort<InitializeContextOutput>("").WithConnectorUI(PortConnectorUI.Arrowhead).Build();
+        }
+    }
+
+    [Serializable]
+    public class Update : ContextNode
+    {
+        public struct UpdateContextOutput
+        {
+        }
+
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddInputPort<Initialize.InitializeContextOutput>("").WithConnectorUI(PortConnectorUI.Arrowhead)
+                .Build();
+            context.AddOutputPort<UpdateContextOutput>("").WithConnectorUI(PortConnectorUI.Arrowhead).Build();
+        }
+    }
+
+    [Serializable]
+    public class Output : ContextNode
+    {
+        public struct OutputContextOutput
+        {
+        }
+
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddInputPort<Update.UpdateContextOutput>("").WithConnectorUI(PortConnectorUI.Arrowhead).Build();
+        }
+    }
+
+    [UseWithContext(typeof(Initialize), typeof(Update), typeof(Output))]
+    [Serializable]
+    public class SetPosition : BlockNode
+    {
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            context.AddInputPort<Vector3>("position").WithDefaultValue(Vector3.zero).Build();
+        }
+    }
+}
