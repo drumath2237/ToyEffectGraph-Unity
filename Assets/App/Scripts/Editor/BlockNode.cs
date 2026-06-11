08aa55d735046104e90009ab6cad17e4c707117b
drumath2237 <2237k.drumath@gmail.com>
2026-06-12 03:11:35 +0900

    :sparkles: add evaluate func to position node

--- Assets/App/Scripts/Editor/BlockNode.cs
+++ Assets/App/Scripts/Editor/BlockNode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using JetBrains.Annotations;
 using Unity.GraphToolkit.Editor;
 using UnityEngine;
 
@@ -6,11 +7,24 @@ namespace ToyEffectGraph.Editor
 {
     [UseWithContext(typeof(Initialize), typeof(Update), typeof(Output))]
     [Serializable]
-    public class SetPosition : BlockNode
+    public class SetPosition : BlockNode, IEvaluatableExpression
     {
+        [NotNull]
+        public readonly string PositionPortName = "positionPort";
+
         protected override void OnDefinePorts(IPortDefinitionContext context)
         {
-            context.AddInputPort<Vector3>("position").WithDefaultValue(Vector3.zero).Build();
+            context.AddInputPort<Vector3>(PositionPortName)
+                .WithDisplayName("position")
+                .WithDefaultValue(Vector3.zero)
+                .Build();
+        }
+
+        public string EvaluateExpression()
+        {
+            var connectedNode =
+                GetInputPortByName(PositionPortName).FirstConnectedPort.GetNode() as IEvaluatableExpression;
+            return $"_Position[i] = ( {connectedNode.EvaluateExpression()} )";
         }
     }
 }
