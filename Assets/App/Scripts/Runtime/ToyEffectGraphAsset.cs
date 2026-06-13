61cf23c2bd594a69ed4659f9967acaa7e2e08c3f
drumath2237 <2237k.drumath@gmail.com>
2026-06-14 03:58:35 +0900

    :sparkles: add scriptable obj of toyvfx

---
+++ Assets/App/Scripts/Runtime/ToyEffectGraphAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace App.Scripts.Runtime
+{
+    public class ToyEffectGraphAsset : ScriptableObject
+    {
+        public string hlslCode;
+        public ComputeShader computeShader;
+    }
+}
