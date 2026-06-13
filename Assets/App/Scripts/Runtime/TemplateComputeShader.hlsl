0efe2886ccd365b5bbfa11a14d42f1a9180e648d
drumath2237 <2237k.drumath@gmail.com>
2026-06-13 22:21:58 +0900

    :bento: add test template hlsl

---
+++ Assets/App/Scripts/Runtime/TemplateComputeShader.hlsl
@@ -0,0 +1,31 @@
+﻿#pragma kernel initialize
+#pragma kernel update
+
+struct Particle
+{
+    float3 position;
+    // float3 velocity;
+    // float lifetime;
+    // float age;
+};
+
+RWStructuredBuffer<Particle> particles;
+
+// float delta_time;
+float time;
+
+[numthreads(64, 1, 1)]
+void initialize(uint id : SV_DispatchThreadID)
+{
+    Particle p;
+
+    particles[id] = p;
+}
+
+[numthreads(64, 1, 1)]
+void update(uint id : SV_DispatchThreadID)
+{
+    Particle p = particles[id];
+
+    particles[id] = p;
+}
